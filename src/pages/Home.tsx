7d445f745c2b37ae487e7b448e05674d3fb9611a
Anas-Sd <anasannu44455@gmail.com>
2026-03-03 12:53:17 +0530

    ENHANCED SEO

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Search, Package, ArrowRight } from 'lucide-react'
+import { Search, ArrowRight } from 'lucide-react'
 
 export default function Home() {
     return (
